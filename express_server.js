66cd06ceb2dfb204e643e9a432544e63e8453842
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-02 15:31:19 -0400

    define the route will match this POST request and generate the random function

--- express_server.js
+++ express_server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  const alphaN = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const amount = 6;
+  let output = "";
+  for (let i = 0; i < amount; i++) {
+    let CharIndex = Math.floor(Math.random() * alphaN.length);
+    output += alphaN[CharIndex];
+  }
+  return output;
+}
+generateRandomString();
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -35,6 +49,11 @@ app.get("/urls/:shortURL", (req, res) => {
   const longURL = urlDatabase[shortURL];
   const templateVars = { shortURL, longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {

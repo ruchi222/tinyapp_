d853253cfc407220dd329c3341ad9b211292ef7d
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-05 18:01:38 -0400

    make the changes in get /u/:id routes

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const cookieSession = require('cookie-session');
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -9,6 +10,7 @@ const { getUserByEmail, generateRandomString, urlsForUser }  = require('./helper
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(cookieSession({ name: 'session', keys: ["TinyApp"]}));
 
 // const urlDatabase = {
 //   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -39,105 +41,74 @@ const users = {
   }
 }
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.get("/", (req, res) => {
   res.send(" Welcome to TinyApp Project!");
 });
 
-// Create a Login page
-
-app.get("/login", (req, res) => {
-  const userID = req.cookies["user_id"];
-  const user = users[userID]
-  let templateVars = { user: user };
-  res.render("urls_login", templateVars);
-});
-
-// POST - Handle a POST route
-
-app.post("/login", (req, res) => {
-  const email  = req.body.login;
-  const password = req.body.password;
-  if (email === 0 || password === 0) {
-    let templateVars = { status: 403, message: "Email or Password is not valid, Please Register!!!", user: undefined } 
-    return res.render("urls_error", templateVars);
-  }
-  const user = getUserByEmail(email, users);
-  if (!user || password !== user.password) {
-    let templateVars = { status: 403, message: "User or Password is not match!!!" , user: undefined } 
-    return res.render("urls_error", templateVars);
-  }
-  res.cookie('user_id', user.id);
-  res.redirect("/urls");
-});
-
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
-
 app.get("/urls", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session["user_id"];
   const user = users[userID]
-  const templateVars = { urls: urlDatabase, user: user };
+  const templateVars = { urls: urlDatabase, user: user }
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session["user_id"];
   const user = users[userID]
   let templateVars = { user: user }
   res.render("urls_new", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-  const userID = req.cookies["user_id"];
-  const user = users[userID]
-  const templateVars = { shortURL, longURL, user: user};
-  res.render("urls_show", templateVars);
-});
-
-
-
-app.post("/urls", (req, res) => {
-  let tempURL = generateRandomString(); 
-  urlDatabase[tempURL] = req.body.longURL
-  res.redirect('/urls') 
-});
-
-// Post - Update the URL
-
-app.post("/urls/:id", (req, res) => {
-  const shortURL = req.params.id; 
-  const newLongURL = req.body.longURL;
-  const user = req.cookies.user_id;
-  if (!user) {
-    return res.status(400).send("You are not user or you are not logged!");
-  } 
-  if (urlDatabase[req.params.id].userID !== user) {
-    return res.status(400).send("These URL is not belong to you!");
-  } 
-    else {
-  urlDatabase[shortURL] = newLongURL;
-  res.redirect("/urls")
-    }
-});
-
 // Get Register
 
 app.get("/register", (req,res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session["user_id"];
   const user = users[userID]
   let templateVars = {
     'urls' : urlDatabase,
     user: user,
   };
   res.render("urls_register", templateVars);
+});
+
+// Create a Login page
+
+app.get("/login", (req, res) => {
+  const userID = req.session["user_id"];
+  const user = users[userID]
+  let templateVars = { user: user };
+  res.render("urls_login", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const userID = req.session["user_id"];
+  const user = users[userID]
+  const templateVars = { shortURL, longURL, user: user};
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]){
+  const longURL = urlDatabase[req.params.shortURL]["longURL"];
+  if (longURL === undefined) {
+    let templateVars = { status: 302, message: "Email or Password is Empty!!!"} 
+    res.render("urls_error", templateVars);
+  } else {
+    res.redirect(longURL);
+  }
+}
+});
+
+app.post("/urls", (req, res) => {
+  let tempURL = generateRandomString(); 
+  urlDatabase[tempURL] = { longURL: req.body.longURL, userID: req.session["user_id"] }
+  res.redirect('/urls') 
 });
 
 // POST - Create a Registration Handler
@@ -166,6 +137,36 @@ app.post("/register", (req,res) => {
   }
 });
 
+// POST - Handle a Login route
+
+app.post("/login", (req, res) => {
+  const email  = req.body.login;
+  const password = req.body.password;
+
+  if (email === 0 || password === 0) {
+    let templateVars = { status: 403, message: "Email or Password is not valid, Please Register!!!", user: undefined } 
+    return res.render("urls_error", templateVars);
+  }
+  const user = getUserByEmail(email, users);
+  if (!user) {
+    let templateVars = { status: 403, message: "User or Password is not match!!!" , user: undefined } 
+    return res.render("urls_error", templateVars);
+  }
+  if (!bcrypt.compareSync(password, user["password"])) {
+    let templateVars = { status: 403, message: "User or Password is not match!!!" , user: undefined } 
+    return res.render("urls_error", templateVars);
+  }
+  req.session['user_id'] = user.id;
+  res.redirect("/urls");
+});
+
+// Post Logout
+
+app.post("/logout", (req, res) => {
+  req.session['user_id'] = null;
+  res.redirect('/login');
+});
+
 // Delete - handle the POST requests on the server
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -174,11 +175,22 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-// Post Logout
+// Post - Update the URL
 
-app.post("/logout", (req, res) => {
-  res.clearCookie ('user_id');
-  res.redirect('/login');
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id; 
+  const newLongURL = req.body.longURL;
+  const user = req.session.user_id;
+  if (!user) {
+    return res.status(400).send("You are not user or you are not logged!");
+  } 
+  if (urlDatabase[req.params.id].userID !== user) {
+    return res.status(400).send("These URL is not belong to you!");
+  } 
+    else {
+  urlDatabase[shortURL] = newLongURL;
+  res.redirect("/urls")
+    }
 });
 
 app.listen(PORT, () => {

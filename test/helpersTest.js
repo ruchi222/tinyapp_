04432e6faf247f1339ded193d5dc9dae6fbf0250
Ruchika Gosain <ruchikagosain@Ruchikas-MacBook-Air.local>
2021-08-06 15:03:57 -0400

    Tested id in getUserByEMail

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, generateRandomString, urlsForUser, cookieHasUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -15,27 +15,31 @@ const testUsers = {
   }
 };
 
+describe('generateRandomString', function() {
+
+  it('should return a string with six characters', function() {
+    const randomStringLength = generateRandomString().length;
+    const expectedOutput = 6;
+    assert.equal(randomStringLength, expectedOutput);
+  });
+
+  it('should not return the same string when called multiple times', function() {
+    const firstRandomString = generateRandomString();
+    const secondRandomString = generateRandomString();
+    assert.notEqual(firstRandomString, secondRandomString);
+  });
+});
+
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedOutput = "userRandomID";
-    // Write your assert statement here
-    assert.equal(user.id , expectedOutput.id);
-    assert.equal(user.email , expectedOutput.email);
-    assert.equal(user.password , expectedOutput.password);
-  });
-
-  
-    it('should return a user with valid email', function() {
-      const user = getUserByEmail("user2@example.com", testUsers)
-      const expectedOutput = { id: "user2RandomID", "email": "user2@example.com", password: "dishwasher-funk" };
-      assert.equal(user.id , expectedOutput.id);
-      assert.equal(user.email , expectedOutput.email);
-      assert.equal(user.password , expectedOutput.password);
-    });
+    assert(user, expectedOutput);
+});
 
     it('should return undefined with invalid email', function() {
         const user = getUserByEmail("test@example.com", testUsers)
-        assert.equal(user, undefined);
+        const expectedOutput = undefined;
+        assert.isUndefined(user, expectedOutput);
     });
 });
